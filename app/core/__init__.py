5df062cd99344194c48ac3656f4aeb18080fe5c3
Shalom <shalomarbsie00@gmail.com>
2026-07-13 09:24:28 +0300

    Merged local feat/testing to upstream/refactor/folder-structure

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
-from .config import Config, create_dependencies
 from .logging_config import setup_logging
 from .socketio_instance import sio
 from .status_tracker import StatusTracker, TaskState, status_tracker
+from .deps import create_dependencies
+from .config import get_settings, Settings
 from .utils import (
     allowed_file,
     compute_file_md5,
@@ -15,7 +15,8 @@ from .utils import (
 )
 
 __all__ = [
-    "Config",
+    "Settings",
+    "get_settings",
     "create_dependencies",
     "setup_logging",
     "sio",
@@ -31,37 +32,3 @@ __all__ = [
     "transform_credible_sets_to_locuszoom",
     "convert_variants_to_object_array",
 ]
-=======
-from .config import Config, create_dependencies
-from .logging_config import setup_logging
-from .socketio_instance import sio
-from .status_tracker import StatusTracker, TaskState, status_tracker
-from .utils import (
-    allowed_file,
-    compute_file_md5,
-    emit_task_update,
-    get_shared_temp_dir,
-    serialize_datetime_fields,
-    get_deps,
-    transform_credible_sets_to_locuszoom,
-    convert_variants_to_object_array,
-)
-
-__all__ = [
-    "Config",
-    "create_dependencies",
-    "setup_logging",
-    "sio",
-    "StatusTracker",
-    "TaskState",
-    "status_tracker",
-    "allowed_file",
-    "compute_file_md5",
-    "emit_task_update",
-    "get_shared_temp_dir",
-    "serialize_datetime_fields",
-    "get_deps",
-    "transform_credible_sets_to_locuszoom",
-    "convert_variants_to_object_array",
-]
->>>>>>> f3cb52fd525a011ec3b1dfef4a6865f709054b6d
